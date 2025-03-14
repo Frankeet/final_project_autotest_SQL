da3664bef2372cade2c774008790462fb3852ce6
Franleet <karpychev05@gmail.com>
2025-03-14 23:55:54 +0300

    Доработка

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,10 +8,6 @@ def post_new_order(body):
                          json=body,
                          headers=data.headers)
 
-# Функция получения номера заказа
-def get_order_number(order_body):
-    return post_new_order(order_body).json()['track']
-
 # Функция получения заказа по его номеру
-def get_order_by_number(order_body):
-   return requests.get(configuration.URL_SERVICE + configuration.GET_ORDERS_PATH + str(get_order_number(order_body)))
+def get_order_by_number(track_number):
+   return requests.get(configuration.URL_SERVICE + configuration.GET_ORDERS_PATH + str(track_number))
